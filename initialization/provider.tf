6a1670ed24e6e18efd7ed0d5b904f268378adfce
Sudhams Reddy Duba <support@way2techstack.com>
2025-08-25 07:59:16 +0530

    Terraform initialization

---
+++ initialization/provider.tf
@@ -0,0 +1,14 @@
+# Terraform Settings block (HCL)
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.10.0"
+    }
+  }
+}
+
+# Provider block
+provider "aws" {
+  # Configuration options
+}
